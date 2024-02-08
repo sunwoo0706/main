637ba87cf5f3d5be95f89053e3334e41a5e6d981
sunwoo0706 <sunwoo0706@icloud.com>
2024-02-08 14:45:08 +0900

    feat: add paragraph wrapper

--- components/window-size-indicator.tsx
+++ components/window-size-indicator.tsx
@@ -6,8 +6,11 @@ export function WindowSizeIndicator() {
   const { width, height } = useWindowSize()
 
   return (
-    <div className="text-white fixed bottom-1 left-1 z-50 p-6 font-mono text-xs">
-      {width}x{height}
+    <div className="fixed bottom-1 left-1 z-50 p-6">
+      <p className="text-white font-mono text-xs">
+        {width}x{height}
+      </p>
+      <span className="sr-only">window size indicator</span>
     </div>
   )
 }
