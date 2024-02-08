a3faf47ff39f964c6317a4b22b6addc71a2c5f25
sunwoo0706 <sunwoo0706@icloud.com>
2024-02-08 19:30:02 +0900

    feat: update external link icon vertical align super

--- components/more.tsx
+++ components/more.tsx
@@ -6,14 +6,15 @@ export function AboutMore() {
   return (
     <section className="-mb-3 mt-16 sm:mt-32">
       <h3 className="mb-5 block font-medium text-gray-1100 sm:mb-4">더보기</h3>
-      <span className="flex items-center gap-px text-gray-1000">
+      <span className="text-gray-1000">
         <a
-          className="flex items-center gap-0.5 underline decoration-1 underline-offset-2 transition"
+          className="underline decoration-1 underline-offset-2 transition"
           href={siteConfig.links.github}
           target="_blank"
           rel="noreferrer"
         >
-          GitHub <Icons.externalLink size={16} />
+          GitHub
+          <Icons.externalLink className="inline-block align-super" size={12} />
         </a>{" "}
         에서 제가 작성한 코드들을 보실 수 있습니다.
       </span>
