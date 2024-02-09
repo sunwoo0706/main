64cd277875a33a88c1e656432644d183802ca8a5
sunwoo0706 <sunwoo0706@icloud.com>
2024-02-09 13:32:44 +0900

    feat: update text color

--- components/section-card.tsx
+++ components/section-card.tsx
@@ -8,12 +8,12 @@ export function SectionCard({ title, description, link }: SectionCardProps) {
   return (
     <a
       href={link}
-      className="dark:hover:bg-gray-200 -mx-3 flex flex-col rounded-md px-3 no-underline transition ease-in-out hover:bg-[#F5F4F4] sm:py-3"
+      className="dark:hover:bg-gray-200 -mx-3 flex flex-col rounded-md px-3 no-underline transition ease-in-out hover:bg-foreground sm:py-3"
       target="_blank"
       rel="noreferrer"
     >
       <span>{title}</span>
-      <span className="text-gray-1100">{description}</span>
+      <span className="text-gray-1000">{description}</span>
     </a>
   )
 }
