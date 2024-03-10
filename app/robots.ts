31ecbae27f2043ed980d78c6082c24bd742f2168
sunwoo0706 <sunwoo0706@icloud.com>
2024-03-10 20:34:55 +0900

    fix: wrong sitemap url

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://acme.com/sitemap.xml",
+    sitemap: "https://sunwoo.xyz/sitemap.xml",
   }
 }
