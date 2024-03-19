a3cb9dedb79252ae5bb42b68a285bbb33d251d23
sunwoo0706 <sunwoo0706@icloud.com>
2024-03-19 21:19:38 +0900

    fix: robots.txt allow field

--- app/robots.ts
+++ app/robots.ts
@@ -1,10 +1,12 @@
 import { MetadataRoute } from "next"
 
+import { siteConfig } from "@/config/site"
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
+      allow: siteConfig.locales.map((locale) => `/${locale}`),
     },
     sitemap: "https://sunwoo.xyz/sitemap.xml",
   }
